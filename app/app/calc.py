02af6c32e3aa73bfdb7b81e5c0499262585dc74f
A7med7777 <ahmedhessian56@gmail.com>
2025-02-04 22:20:24 +0200

    Test Driven Development with Django and Configure Database

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def sub(x, y):
+    """Subtractig x for y and return the result"""
+    return y - x
