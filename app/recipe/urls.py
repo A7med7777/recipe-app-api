334de9f0b033378574885c1a1c23458a1a11379f
A7med7777 <ahmedhessian56@gmail.com>
2025-03-13 14:43:25 +0200

    Build ingredients API

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -3,10 +3,11 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from recipe.views import RecipeViewSet, TagViewSet
+from recipe.views import RecipeViewSet, TagViewSet, IngredientViewSet
 
 router = DefaultRouter()
 router.register("recipes", RecipeViewSet)
 router.register("tags", TagViewSet)
+router.register("ingredients", IngredientViewSet)
 app_name = "recipe"
 urlpatterns = [path("", include(router.urls))]
